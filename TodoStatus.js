02829f46e6513c1fa9c079860dc338f95fb50cea
Surafel Getachew <suragetch@gmail.com>
2022-08-18 18:55:55 +0300

    Add the status to to do

---
+++ TodoStatus.js
@@ -0,0 +1,20 @@
+import { todos, updateUI } from "./ModifyTodos.js";
+
+const changeTodoStatus = ({ index, status }) => {
+  todos[index - 1].completed = status;
+  localStorage.setItem("todos", JSON.stringify(todos));
+  updateUI();
+};
+const removeCompletedTodos = () => {
+  const newTodos = todos
+    .filter((element) => element.completed !== true)
+    .map((element, index) => {
+      element.index = index + 1;
+      return element;
+    });
+  localStorage.setItem("todos", JSON.stringify(newTodos));
+  updateUI(newTodos);
+  return newTodos;
+};
+
+export { changeTodoStatus, removeCompletedTodos };
